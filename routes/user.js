5699c8c377fb5114b162777480618c7083661736
Emilsboq <emil.olsson1@jgy.se>
2020-02-25 16:22:53 +0100

    added route to get database info

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../db/dbConn");
 
 
 router.get("/api/user", (req, res) => {
@@ -8,6 +9,26 @@ router.get("/api/user", (req, res) => {
 
 router.get("/api/user/all", (req, res) => {
     res.send("<h1>Hello All Users</h>");
+
+    
+
+});
+
+router.get("/api/users/all", (req, res) => {
+    let queryString = "SELECT * FROM blog_db ORDER BY author DESC";
+    db().query(queryString, (error, rows, fields) => {
+        
+        if(error) {
+            res.sendStatus(500);
+            res.end();
+            return;
+        };
+        
+       const data = rows.map( (row) => {
+           return {Id: row.id, author: row.author, body: row.body, created: row.created, title: row.title};
+       });
+       res.json(data);
+    });
 });
 
 module.exports = router;
