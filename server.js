37f8bd1f56e20917345f4c77d8deae94851438d0
Emilsboq <emil.olsson1@jgy.se>
2020-02-26 11:18:22 +0100

    Tweaked code

--- server.js
+++ server.js
@@ -5,29 +5,33 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 
-
 const app = express();
+
+// Route imports
+const postsRoute = require("./routes/posts");
+const homeRoute = require("./routes/home");
+// Variables
 const PORT = (process.env.PORT || 5000);
 
+// App
 app.use(helmet());
-app.use(bodyParser.json({type : 'application/*+json'}));
+app.use(bodyParser.json({ type: 'application/*+json' }));
 
+// Logging
 app.use(morgan("dev"));
 
+// 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
-const homeRoute = require("./routes/home");
+// Routes
+app.use(postsRoute);
 app.use(homeRoute);
 
-const userRoute = require("./routes/user");
-app.use(userRoute);
-
-
-
+// Start the server.
 app.listen(PORT, () => {
-    console.log("Server started on port : " + PORT);
-}); 
+    console.log("Server started on port " + PORT);
+});
