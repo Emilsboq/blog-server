eb473d6a2128a463fb779dd169a25c7ad9544d61
Emilsboq <emil.olsson1@jgy.se>
2020-02-27 14:04:51 +0100

    changed user and password

--- db/dbConn.js
+++ db/dbConn.js
@@ -5,8 +5,8 @@ module.exports = function getConnection() {
         {
             connectionLimit: 10,
             host: process.env.HOST || "127.0.0.1",
-            user: process.env.USER || "user",
-            password: process.env.PASSWORD || "password",
+            user: process.env.USER || "localhost",
+            password: process.env.PASSWORD || "lösenord",
             database: process.env.DATABASE || "blog_db"
         }
     );
